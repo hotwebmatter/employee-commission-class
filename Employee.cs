e25a0f5c82b37e1951cd65bee30617da548c68d3
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-04 17:18:25 -0500

    Declare appropriate constants

--- Employee.cs
+++ Employee.cs
@@ -6,5 +6,10 @@ namespace employee_commission_class
 {
     class Employee
     {
+        // constants
+        const double COMMISSION = .07,
+                     FEDERAL_TAX = .18,
+                     RETIREMENT = .1,
+                     SOCIAL_SECURITY = .06;
     }
 }
