96f1d6500d28502c50d9eff373797c6b04a72d2e
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-04 17:12:13 -0500

    Initial commit of employee-commission-class project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace employee_commission_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
